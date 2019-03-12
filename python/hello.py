b9b8596f9a3bf3935ee050e01ca88b136d233e3b
new2ind <new2ind@gmail.com>
2019-03-12 14:04:42 +0900

    1

--- python/hello.py
+++ python/hello.py
@@ -3,12 +3,4 @@ import datetime
 curDay = datetime.date.today().day
 curMonth = datetime.date.today().month
 
-<<<<<<< HEAD
 print(str(curMonth) + 'm' + str(curDay) + 'd')
-=======
-c = a + b
-
-print(c
-
-print('o11k')
->>>>>>> f188d20176d3e15666b90e30130b81ca5749b96c
